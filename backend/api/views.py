2b84e869161375a030555c6d6deaf3d6b02c5718
AbdoEldgwy <abdo.eldgwy1@gmail.com>
2025-02-03 02:19:22 +0200

    how to Echo to django local server

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+import json
 from django.http import JsonResponse
 
 def api_home(request,*arge,**kwargs):
-    return JsonResponse({'message': 'Welcome to the API'})
+    body  = request.body
+    data = {}
+    print("Get:",request.GET)
+    print("Post:",request.POST)
+    try:
+        data = json.loads(body)   # converting the request body [string json data] --> json type
+    except:
+        pass 
+      
+    data["params"] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print("data:",data)
+
+    return JsonResponse({'message': 'Welcome to the API',
+                          'data': data})
   
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,14 @@
 import requests
 
 endpoits = "https://httpbin.org/anything"
-endpoits = "http://127.0.0.1:8000/"
+endpoits = "http://127.0.0.1:8000/api/"
   
-get_response = requests.get(endpoits, json={"query":"hello world"})
-print(get_response.text) 
+get_response = requests.get(endpoits,params ={"abc":123}, json={"query":"hello world"})  
 
 
+# print(get_response.text) 
+# print(get_response.status_code)
+
+print(get_response.json())
+
+# ============================== 
